6aced72f81a02db9bb5d1a1fb98bd9bde50c4652
upendra <upendrauddagiri030@gmail.com>
2026-02-15 11:52:26 +0530

    Added my project folder

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
-import pandas
+from tkinter import *
+import requests
 
-data = pandas.read_csv("nato_phonetic_alphabet.csv")
+def get_quote():
+    data=requests.get(url="https://api.kanye.rest")
+    quote=data.json()["quote"]
+    canvas.itemconfig(quote_text,text=f"{quote}")
 
-phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-print(phonetic_dict)
 
-def generate_phonetic():
-    word = input("Enter a word: ").upper()
-    try:
-        output_list = [phonetic_dict[letter] for letter in word]
-    except KeyError:
-        print("Sorry, only letters in the alphabet please.")
-        generate_phonetic()
-    else:
-        print(output_list)
 
-generate_phonetic()
+window = Tk()
+window.title("Kanye Says...")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(width=300, height=414)
+background_img = PhotoImage(file="background.png")
+canvas.create_image(150, 207, image=background_img)
+quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
+canvas.grid(row=0, column=0)
+
+kanye_img = PhotoImage(file="kanye.png")
+kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
+kanye_button.grid(row=1, column=0)
+
+
+
+window.mainloop()
